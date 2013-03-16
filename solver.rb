58819c29d3878ac3722815f84da3cfef9d5d071e
Jay Strybis <jay.strybis@gmail.com>
2013-03-16 16:03:43 -0500

    remove whitespace

--- solver.rb
+++ solver.rb
@@ -2,84 +2,84 @@ require 'rubygems'
 
 class Solver
 
-	attr_reader :goalRow1, :goalRow2, :goalRow3
-	attr_reader :row1, :row2, :row3
-	
-	def initialize(row1, row2, row3)
-		@goalRow1 = [1,2,3]
-		@goalRow2 = [4,5,6]
-		@goalRow3 = [7,8,0]
-		
-		@row1 = row1
-		@row2 = row2
-		@row3 = row3
-	end
-	
-	def goalArray
-		[@goalRow1, @goalRow2, @goalRow3]
-	end
-	
-	def startArray
-		[@row1, @row2, @row3]
-	end
-	
-	def md
-		sum = 0
-		startArray.flatten.each do |v|
-			sum += distance(v)
-		end
-		
-		sum
-	end
-	
-	def mdArray
-		[[2,1,0], [2,1,1], [2,2,1]]
-	end
-	
-	def distance(num)
-		locationGoal = location(num, goalArray)
-		locationStart = location(num, startArray)
-		distanceAway(locationGoal, locationStart)
-		
-	end
-	
-	def location(num, targetArray)
-		targetArray.each_index do |row_index|
-			targetArray[row_index].each_index do |col|
-				return [row_index, col] if targetArray[row_index][col] == num
-			end
-		end
-	end
-	
-	def distanceAway(goalarr, startarr)
-		(startarr[0] - goalarr[0]).abs + (startarr[1] - goalarr[1]).abs
-	end
-	
-	def can_move_left?
-		location(0, startArray)[1] > 0
-	end
-	
-	def can_move_right?
-		location(0, startArray)[1] < 2
-	end
-	
-	def can_move_up?
-		location(0, startArray)[0] > 0
-	end
-	
-	def can_move_down?
-		location(0, startArray)[0] < 2
-	end
-	
-	def possible_moves
-		arr = []
-		arr.push(:left) if can_move_left?
-		arr.push(:right) if can_move_right?
-		arr.push(:down) if can_move_down?
-		arr.push(:up) if can_move_up?
-		arr.sort	
-	end
-	
+  attr_reader :goalRow1, :goalRow2, :goalRow3
+  attr_reader :row1, :row2, :row3
+
+  def initialize(row1, row2, row3)
+    @goalRow1 = [1,2,3]
+    @goalRow2 = [4,5,6]
+    @goalRow3 = [7,8,0]
+
+    @row1 = row1
+    @row2 = row2
+    @row3 = row3
+  end
+
+  def goalArray
+    [@goalRow1, @goalRow2, @goalRow3]
+  end
+
+  def startArray
+    [@row1, @row2, @row3]
+  end
+
+  def md
+    sum = 0
+    startArray.flatten.each do |v|
+      sum += distance(v)
+    end
+
+    sum
+  end
+
+  def mdArray
+    [[2,1,0], [2,1,1], [2,2,1]]
+  end
+
+  def distance(num)
+    locationGoal = location(num, goalArray)
+    locationStart = location(num, startArray)
+    distanceAway(locationGoal, locationStart)
+
+  end
+
+  def location(num, targetArray)
+    targetArray.each_index do |row_index|
+      targetArray[row_index].each_index do |col|
+        return [row_index, col] if targetArray[row_index][col] == num
+      end
+    end
+  end
+
+  def distanceAway(goalarr, startarr)
+    (startarr[0] - goalarr[0]).abs + (startarr[1] - goalarr[1]).abs
+  end
+
+  def can_move_left?
+    location(0, startArray)[1] > 0
+  end
+
+  def can_move_right?
+    location(0, startArray)[1] < 2
+  end
+
+  def can_move_up?
+    location(0, startArray)[0] > 0
+  end
+
+  def can_move_down?
+    location(0, startArray)[0] < 2
+  end
+
+  def possible_moves
+    arr = []
+    arr.push(:left) if can_move_left?
+    arr.push(:right) if can_move_right?
+    arr.push(:down) if can_move_down?
+    arr.push(:up) if can_move_up?
+    arr.sort	
+  end
+
 end
 
 
--- solver_test.rb
+++ solver_test.rb
@@ -4,88 +4,88 @@ require './solver'
 
 class SolverTest < Test::Unit::TestCase
 
-	def setup
-		@solver = Solver.new([7,5,3],[2,8,0], [1,4,6])
-	end
-	test "this should fail" do
-		assert_equal 0,0
-	end
-	
-	test "the solver have a goal array" do
-		assert_equal [1,2,3] , @solver.goalRow1
-		assert_equal [4,5,6] , @solver.goalRow2
-		assert_equal [7,8,0] , @solver.goalRow3
-	end
-	
-	test "sample start" do
-		assert_equal [7,5,3] , @solver.row1
-		assert_equal [2,8,0] , @solver.row2
-		assert_equal [1,4,6] , @solver.row3
-	end
-	
-	test "goal array" do
-		assert_equal [[1,2,3], [4,5,6], [7,8,0]], @solver.goalArray
-	end
-	
-	test "sample array" do 
-		assert_equal [[7,5,3], [2,8,0], [1,4,6]], @solver.startArray
-	end
-	
-	test "distice in arrays" do
-		assert_equal 12, @solver.md
-	end
-	
-	test "md array" do
-		assert_equal [[2,1,0], [2,1,1], [2,2,1]], @solver.mdArray
-	end
-	
-	test "number distance" do
-		assert_equal 2, @solver.distance(7)
-		assert_equal 0, @solver.distance(3)
-		assert_equal 1, @solver.distance(5)
-		assert_equal 2, @solver.distance(2)
-		assert_equal 1, @solver.distance(8)
-		assert_equal 1, @solver.distance(0)
-		assert_equal 2, @solver.distance(1)
-		assert_equal 2, @solver.distance(4)
-		assert_equal 1, @solver.distance(6)
-	end
-	
-	test "location method" do
-		assert_equal [2,1], @solver.location(4, @solver.startArray)
-		assert_equal [1,0], @solver.location(4, @solver.goalArray)
-	end
-	
-	test "distance away" do 
-		assert_equal 2, @solver.distanceAway([2,1], [1,0])
-	end
-	
-	test "can move left" do
-		assert @solver.can_move_left?
-		@solver = Solver.new([0,5,3],[2,8,7], [1,4,6])
-		assert !@solver.can_move_left?
-	end
-	
-	test "can move right" do
-		assert !@solver.can_move_right?
-		@solver = Solver.new([0,5,3],[2,8,7], [1,4,6])
-		assert @solver.can_move_right?
-	end
-	
-	test "can move up" do
-		assert @solver.can_move_up?
-		@solver = Solver.new([0,5,3],[2,8,7], [1,4,6])
-		assert !@solver.can_move_up?
-	end
-	
-	test "can move down" do
-		assert @solver.can_move_down?
-		@solver = Solver.new([4,5,3],[2,8,7], [1,0,6])
-		assert !@solver.can_move_down?
-	end
-	
-	test "current state possible moves" do
-		assert_equal [:down, :left, :up], @solver.possible_moves
-	end
-	
+  def setup
+    @solver = Solver.new([7,5,3],[2,8,0], [1,4,6])
+  end
+  test "this should fail" do
+    assert_equal 0,0
+  end
+
+  test "the solver have a goal array" do
+    assert_equal [1,2,3] , @solver.goalRow1
+    assert_equal [4,5,6] , @solver.goalRow2
+    assert_equal [7,8,0] , @solver.goalRow3
+  end
+
+  test "sample start" do
+    assert_equal [7,5,3] , @solver.row1
+    assert_equal [2,8,0] , @solver.row2
+    assert_equal [1,4,6] , @solver.row3
+  end
+
+  test "goal array" do
+    assert_equal [[1,2,3], [4,5,6], [7,8,0]], @solver.goalArray
+  end
+
+  test "sample array" do 
+    assert_equal [[7,5,3], [2,8,0], [1,4,6]], @solver.startArray
+  end
+
+  test "distice in arrays" do
+    assert_equal 12, @solver.md
+  end
+
+  test "md array" do
+    assert_equal [[2,1,0], [2,1,1], [2,2,1]], @solver.mdArray
+  end
+
+  test "number distance" do
+    assert_equal 2, @solver.distance(7)
+    assert_equal 0, @solver.distance(3)
+    assert_equal 1, @solver.distance(5)
+    assert_equal 2, @solver.distance(2)
+    assert_equal 1, @solver.distance(8)
+    assert_equal 1, @solver.distance(0)
+    assert_equal 2, @solver.distance(1)
+    assert_equal 2, @solver.distance(4)
+    assert_equal 1, @solver.distance(6)
+  end
+
+  test "location method" do
+    assert_equal [2,1], @solver.location(4, @solver.startArray)
+    assert_equal [1,0], @solver.location(4, @solver.goalArray)
+  end
+
+  test "distance away" do 
+    assert_equal 2, @solver.distanceAway([2,1], [1,0])
+  end
+
+  test "can move left" do
+    assert @solver.can_move_left?
+    @solver = Solver.new([0,5,3],[2,8,7], [1,4,6])
+    assert !@solver.can_move_left?
+  end
+
+  test "can move right" do
+    assert !@solver.can_move_right?
+    @solver = Solver.new([0,5,3],[2,8,7], [1,4,6])
+    assert @solver.can_move_right?
+  end
+
+  test "can move up" do
+    assert @solver.can_move_up?
+    @solver = Solver.new([0,5,3],[2,8,7], [1,4,6])
+    assert !@solver.can_move_up?
+  end
+
+  test "can move down" do
+    assert @solver.can_move_down?
+    @solver = Solver.new([4,5,3],[2,8,7], [1,0,6])
+    assert !@solver.can_move_down?
+  end
+
+  test "current state possible moves" do
+    assert_equal [:down, :left, :up], @solver.possible_moves
+  end
+
 end
